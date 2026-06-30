8cc44ccdf0510ec390d4f41318a2fa0e1b5987cb
Shwetank-Maurya <sd3564086@gmail.com>
2026-06-30 14:30:22 +0530

    added a route to directly check the token that whether that token is valid or not

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const UserService = require('../services/user-service');
+const { RelationshipType } = require('sequelize/lib/errors/database/foreign-key-constraint-error');
 
 const userService = new UserService();
 
@@ -25,6 +26,7 @@ const create = async (req,res) => {
         });
     }
 }
+
 const signIn = async ( req, res) => {
     try {
         const response = await userService.signIn(req.body.email, req.body.password);
@@ -34,7 +36,6 @@ const signIn = async ( req, res) => {
             err:{},
             message:'Successfully created user',
         });
-
     } catch (error) {
         return res.status(500).json({
             message:'Something went wrong',
@@ -46,7 +47,29 @@ const signIn = async ( req, res) => {
     }
 }
 
+const isAuthenticated = async (req,res) => {
+    try {
+        const token = req.headers['x-access-token'];
+        const response = await userService.isAuthenticated(token);
+        return res.status(200).json({
+            success:true,
+            err:{},
+            data:response,
+            message:"User is authenticated and token is valid"
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:'Something went wrong',
+            data:{},
+            success:false,
+            err: error
+        });
+    }
+}
+
 module.exports = {
     create,
     signIn,
+    isAuthenticated,
 }
