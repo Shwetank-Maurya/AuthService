37997b901f365d4617b866bc40b9a79af42318c9
Shwetank-Maurya <sd3564086@gmail.com>
2026-07-01 11:22:31 +0530

    Added user roles and added tables and also many to many and db sync

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const db= require('./models/index')
+const {User, Role} = require('./models/index');
+
 const apiRoutes = require('./routes/index');
 
-const UserService = require('./services/user-service')
+const UserService = require('./services/user-service');
+const { RelationshipType } = require('sequelize/lib/errors/database/foreign-key-constraint-error');
 
 const app = express();
 
@@ -19,6 +23,16 @@ const prepareAndStartServer = () =>{
     app.listen(PORT, async () => {
         console.log(` Server Started at PORT ${PORT} .`);
         
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+
+        // const u1=await User.findByPk(1);
+        // const r1= await Role.findByPk(2);
+        // //u1.addRole(r1);
+        // const respone = await u1.hasRoles(r1);
+        // console.log(respone);
+
         // const service = new UserService();
         // // const newToken = service.createToken({email:'sonali@gmail.com',id:1});
         // // console.log("new token is : ",newToken);
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -45,12 +45,12 @@ class UserService {
             if(!response){
                 throw {error: 'Invalid token'}
             }
-            const user = this.userRepository.getById(response.id);
+            const user = await this.userRepository.getById(response.id);
             if(!user){
                 throw { error: 'No user with the corresponding token exists'};
             }
             return user.id;
-            
+
         } catch (error) {
             console.log("Something went wrong in the auth process");
             throw(error);
